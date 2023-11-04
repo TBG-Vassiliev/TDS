493da121cfdc702b38a6655a123f2e65484def8a
TBG-Vassiliev <guibertu5bgpe11314@gmail.com>
2023-11-04 15:44:25 +0100

    Restructuration et début du main

--- Main.py
+++ Main.py
@@ -1,29 +1,66 @@
+# Import de nos fonctions
+
+from fonctions.essai import *
+# from fonctions.Convolution2 import *
+# from fonctions.Warhol_Flou import *
+# from fonctions.gradient import *
+# from fonctions.interface import *
+# from fonctions.internextern import *
+# from fonctions.marr_hildreth import *
+# from fonctions.proj import *
+# from fonctions.proj2 import *
+
+# Import des autres modules
+
 import tkinter as tk
 from tkinter import ttk
 from ttkthemes import ThemedStyle
+import shutil
+import os
 
-def button_clicked():
-    print("Bouton cliqué")
+
+def button_clicked(button_name):
+    print(f"Bouton {button_name} clique")
 
 root = tk.Tk()
-root.title("Boutons esthétiques")
+root.title("Menu")
 
+# Noms des boutons
+button_names = [
+    "Image originale",
+    "Contours",
+    "Gradients",
+    "Image de bande dessinée",
+    "Image combinée",
+    "Montage flou",
+    "Image flou",
+    "Montage Warhol",
+    "Marr-Hildreth",
+    "Zones interne/externe"
+]
+
+# Utilisation du thème "adapta"
 style = ThemedStyle(root)
-style.set_theme("adapta")  # Choisissez un thème parmi ceux disponibles
+style.set_theme("adapta")
 
-button1 = ttk.Button(root, text="Bouton 1", command=button_clicked)
-button1.pack()
-
-button2 = ttk.Button(root, text="Bouton 2", command=button_clicked)
-button2.pack()
-
-button3 = ttk.Button(root, text="Bouton 3", command=button_clicked)
-button3.pack()
-
-button4 = ttk.Button(root, text="Bouton 4", command=button_clicked)
-button4.pack()
-
-button5 = ttk.Button(root, text="Bouton 5", command=button_clicked)
-button5.pack()
+# Création des boutons avec les noms spécifiés
+for i, name in enumerate(button_names):
+    row = i // 5
+    col = i % 5
+    button = ttk.Button(root, text=name, command=lambda name=name: button_clicked(name), width=20)
+    button.grid(row=row, column=col, padx=5, pady=5)
 
 root.mainloop()
+
+
+
+# Suppression du répertoire __pycache__ à la fin de l'exécution
+directory = 'fonctions/__pycache__'
+
+try:
+    shutil.rmtree(directory)
+    print(f"Le repertoire {directory} a ete supprime.")
+except FileNotFoundError:
+    print(f"Le repertoire {directory} n'existe pas.")
+except Exception as e:
+    print(f"Une erreur s'est produite : {e}")
